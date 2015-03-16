a4af8719e48b85f3b9d91389c3d579b971dd8820
Trent Nelson <trent@trent.me>
2015-03-16 19:04:08 -0400

    Add tornado.

---
+++ tefb_tornado.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import tornado.ioloop
+import tornado.web
+import tornado.httpserver
+
+class BaseHandler(tornado.web.RequestHandler):
+    def compute_etag(self):
+        return None
+
+class JsonHandler(BaseHandler):
+    def get(self):
+        obj = { 'message': 'Hello, World!' }
+        self.write(obj)
+
+class PlaintextHandler(BaseHandler):
+    def get(self):
+        self.set_header('Content-Type', 'text/plain')
+        self.write(b'Hello, World!')
+
+app = tornado.web.Application((
+    ('/json', JsonHandler),
+    ('/plaintext', PlaintextHandler),
+))
+
+def main():
+    server = tornado.httpserver.HTTPServer(app)
+    if os.name == 'nt' or ('dofork' not in sys.argv):
+        server.listen(8080)
+    else:
+        server.bind(8080)
+        server.start(0)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+    main()
